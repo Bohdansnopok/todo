89e913cdf8097fd44515e377f98f081547ef2492
Bohdansnopok <snopokbogdan3@gmail.com>
2026-02-26 18:25:39 +0200

    add top-line at dashboard

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -6,6 +6,7 @@ import avatar2 from "../../../public/images/SearchIcon2.svg";
 import avatar3 from "../../../public/images/SearchIcon3.svg";
 import avatar4 from "../../../public/images/SearchIcon4.svg";
 import avatar5 from "../../../public/images/SearchIcon5.svg";
+import invite from "../../../public/images/invite-icon.svg";
 
 export default function Home() {
   return (
@@ -17,12 +18,31 @@ export default function Home() {
             <Image src={handWave} alt="" className="dashboard__handWave" />
           </div>
 
-          <div className="dashboard__top-line__avatars">
-            <Image src={avatar1} alt="" />
-            <Image src={avatar2} alt="" />
-            <Image src={avatar3} alt="" />
-            <Image src={avatar4} alt="" />
-            <Image src={avatar5} alt="" />
+          <div className="dashboard__invite flex-row">
+            <div className="dashboard__top-line__avatars flex-row">
+              <a href="#" className="dashboard__top-line__avatars__link">
+                <Image src={avatar1} alt="" height={37} width={37} />
+              </a>
+              <a href="#" className="dashboard__top-line__avatars__link">
+                <Image src={avatar2} alt="" height={37} width={37} />
+              </a>
+              <a href="#" className="dashboard__top-line__avatars__link">
+                <Image src={avatar3} alt="" height={37} width={37} />
+              </a>
+              <a href="#" className="dashboard__top-line__avatars__link">
+                <Image src={avatar4} alt="" height={37} width={37} />
+              </a>
+              <div className="dashboard__last-avatar-wrapper dashboard__top-line__avatars__link">
+                <Image src={avatar5} alt="" height={37} width={37} />
+                <span>+4</span>
+                <div className="overlay"></div>
+              </div>
+            </div>
+
+            <button className="dashboard__invite-button flex-row">
+              <Image src={invite} alt="" />
+              Invite
+            </button>
           </div>
         </div>
       </div>
